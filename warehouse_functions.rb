fb8780f8bd9658f48a76437858ba24870fffb219
CallumDougan <practicalitymalfunction@gmail.com>
2015-11-20 16:09:17 +0000

    bahahahahahahahahahahahaha

--- warehouse_functions.rb
+++ warehouse_functions.rb
@@ -1,44 +1,191 @@
-BAYS = [
+BAYS=[
 {
-  letter: "A",
-  number: "1",
-  content: "needle"
+  letter: "a", 
+  number: "10",
+  content: "rubber band"
 },
+
 {
-  letter: "A",
-  number: "2",
-  content: "stop sign"
+  letter: "a", 
+  number: "9",
+  content: "glow stick"
 },
+
 {
-  letter: "A",
+  letter: "a", 
+  number: "8",
+  content: "model car"
+},
+
+{
+  letter: "a", 
+  number: "7",
+  content: "bookmark"
+},
+
+{
+  letter: "a", 
+  number: "6",
+  content: "shovel"
+},
+
+{
+  letter: "a", 
+  number: "5",
+  content: "rubber duck"
+},
+
+{
+  letter: "a", 
+  number: "4",
+  content: "hanger"
+},
+
+{
+  letter: "a", 
   number: "3",
   content: "blouse"
 },
+
 {
-  letter: "",
-  number: "",
-  content: ""
+  letter: "a", 
+  number: "2",
+  content: "stop sign"
 },
+
 {
-  letter: "",
-  number: "",
-  content: ""
+  letter: "a", 
+  number: "1",
+  content: "needle"
 },
+
 {
-  letter: "",
-  number: "",
-  content: ""
+  letter: "c", 
+  number: "1",
+  content: "rusty nail"
+},
+
+{
+  letter: "c", 
+  number: "2",
+  content: "drill press"
+},
+
+{
+  letter: "c", 
+  number: "3",
+  content: "chalk"
+},
+
+{
+  letter: "c", 
+  number: "4",
+  content: "word search"
+},
+
+{
+  letter: "c", 
+  number: "5",
+  content: "thermometer"
+},
+
+{
+  letter: "c", 
+  number: "6",
+  content: "face wash"
+},
+
+{
+  letter: "c", 
+  number: "7",
+  content: "paint brush"
+},
+
+{
+  letter: "c", 
+  number: "8",
+  content: "candy wrapper"
+},
+
+{
+  letter: "c", 
+  number: "9",
+  content: "sharpie"
+},
+
+{
+  letter: "c", 
+  number: "10",
+  content: "leg warmers"
+},
+
+{
+  letter: "b", 
+  number: "1",
+  content: "tyre swing"
+},
+
+{
+  letter: "b", 
+  number: "2",
+  content: "sharpie"
+},
+
+{
+  letter: "b", 
+  number: "3",
+  content: "picture frame"
+},
+
+{
+  letter: "b", 
+  number: "4",
+  content: "photo album"
+},
+
+{
+  letter: "b", 
+  number: "5",
+  content: "nail filer"
+},
+
+{
+  letter: "b", 
+  number: "6",
+  content: "tooth paste"
+},
+
+{
+  letter: "b", 
+  number: "7",
+  content: "bath fizzers"
+},
+
+{
+  letter: "b", 
+  number: "8",
+  content: "tissue box"
+},
+
+{
+  letter: "b", 
+  number: "9",
+  content: "deodorant"
+},
+
+{
+  letter: "b", 
+  number: "10",
+  content: "cookie jar"
 }
+
 ]
 
 #later question re: relative distances - simply make a seperate array!
 
-def baynum_split
-
-end
 
 def item_finder_content(baynum)
-  baynum_split = baynum.upcase.split(//)
+  baynum_split = baynum.downcase.split(//)
   BAYS.select{|x| x[:letter] == baynum_split[0] && x[:number] == baynum_split[1]}.first[:content]
 end
 
@@ -46,11 +193,21 @@ def item_finder_bay (item_name)
   BAYS.select{|x| x[:content] == item_name}.first[:letter] + BAYS.select{|x| x[:content] == item_name}.first[:number]
 end
 
-def item_finder_content_plural (baynum_1, baynum_2, baynum_3)
-
+def item_finder_content_plural(baynum_1, baynum_2, baynum_3)
+  item_finder_content(baynum_1) + " " + item_finder_content(baynum_2)+ " and " +item_finder_content(baynum_3)
 end
 
-def item_finder_bay_plural (item_1, item_2, item_3)
+def item_finder_bay_plural(item_1, item_2, item_3)
+  item_finder_bay(item_1) + " " + item_finder_bay(item_2) + " and " + item_finder_bay(item_3)
+end
 
+def distance_calc(baynum_1, baynum_2, baynum_3)
+items = [item_finder_content(baynum_1), item_finder_content(baynum_2), item_finder_content(baynum_3)]
 
+lowest, highest = items.map { |item| find_bay_index(item) }.minmax
+highest - lowest
+end
+
+def find_bay_index(item) 
+  BAYS.index { |bay| bay[:content] == item }
 end
--- warehouse_runner.rb
+++ warehouse_runner.rb
@@ -1,15 +1,21 @@
 require_relative('./warehouse_functions.rb')
 
 
-found_item_content = item_finder_content("a1")
-puts "item at A1 found is #{found_item_content} - expected to be needle"
+# found_item_content = item_finder_content("a1")
+# puts "item at A1 found is #{found_item_content} - expected to be needle"
 
-found_item_bay = item_finder_bay("needle")
-puts "item is at bay #{found_item_bay}, expected to be A1"
+# found_item_bay = item_finder_bay("needle")
+# puts "item is at bay #{found_item_bay}, expected to be A1"
 
-input_1 = "a1"
-input_2 = "a2"
-input_3 = "a3"
+# input_1 = "a1"
+# input_2 = "a2"
+# input_3 = "a3"
 
-found_plural_items_content = item_finder_content_plural(input_1, input_2, input_3)
-puts "items at #{input_1}, #{input_2}, and #{input_3} are #{found_plural_items_content}, expected to be needle, stop sign, blouse"
+# found_plural_items_content = item_finder_content_plural(input_1, input_2, input_3)
+# puts "items at #{input_1}, #{input_2}, and #{input_3} are #{found_plural_items_content}, expected to be needle, stop sign, blouse"
+
+# found_bays_plural = item_finder_bay_plural("b5", "b10", "b6")
+# puts "bays of assigned items are #{found_bays_plural} - expected A1, 2, 3"
+
+total_distance = distance_calc("b5", "b10", "b6")
+puts "bay products are #{total_distance} bays apart"
